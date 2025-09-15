34fc65b267ff0d56432c51f708ed3409ed0ffcd6
Deluxvalle <deibyvalle2002@gmail.com>
2025-09-15 13:50:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ import discord
 
 TOKEN = os.environ["DISCORD_TOKEN"]
 
+# Configuración de intents
 intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.members = True
+
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f"✅ Bot conectado como {client.user}")
 
+# Inicia el servidor keep-alive
 keep_alive()
+
+# Inicia el bot
 client.run(TOKEN)
